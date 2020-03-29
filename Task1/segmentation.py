682e1e93a418d0ac5f453b55f83ce0b3b2814185
robyjacob <robyjacob1998@gmail.com>
2020-03-29 13:35:14 +0530

    Initial commit

---
+++ Task1/segmentation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('women.jpg')
+mask = np.zeros(img.shape[:2],np.uint8)
+
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+
+rect = (100,100,480,500)
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis]
+
+plt.imshow(img),plt.colorbar(),plt.show()
+#newmask = cv2.imread('newmask.png',0)
+
+# whereever it is marked white (sure foreground), change mask=1
+# whereever it is marked black (sure background), change mask=0
+#mask[newmask == 0] = 0
+#mask[newmask == 255] = 1
+
+#mask, bgdModel, fgdModel = cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
+
+#mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+#img = img*mask[:,:,np.newaxis]
+#plt.imshow(img),plt.colorbar(),plt.show()
