d5c77d8b8bbcf757dd31490b62fa623c6c4df9a4
robyjacob <robyjacob1998@gmail.com>
2020-06-14 16:29:04 +0530

    modified code to test multiple images simultaneously

--- Task3/run_placesCNN_basic.py
+++ Task3/run_placesCNN_basic.py
@@ -4,6 +4,7 @@
 # last modified by Bolei Zhou, Dec.27, 2017 with latest pytorch and torchvision (upgrade your torchvision please if there is trn.Resize error)
 
 import torch
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
 from torch.autograd import Variable as V
@@ -49,20 +50,23 @@ with open(file_name) as class_file:
         classes.append(line.strip().split(' ')[0][3:])
 classes = tuple(classes)
 
-# load the test image
-img = Image.open('test3.jpg')
-# plt.imshow(img); plt.show()
-input_img = V(centre_crop(img).unsqueeze(0))
+img_names = glob.glob("*.jpg")
+test_imgs = []
+for img_name in img_names:
+    # load the test image
+    img = Image.open(img_name)
+    # plt.imshow(img); plt.show()
+    test_img = V(centre_crop(img).unsqueeze(0))
+    test_imgs.append(test_img)
 
-
-# forward pass
-logit = model.forward(input_img)
-h_x = F.softmax(logit,1).data.squeeze()
-probs, idx = h_x.sort(0, True)
-max_prob_id = torch.argmax(probs)
-
-# display accuracy
-print(probs[max_prob_id].item(), classes[idx[max_prob_id]])
+for img in test_imgs: 
+    # forward pass
+    logit = model.forward(img)
+    h_x = F.softmax(logit,1).data.squeeze()
+    probs, idx = h_x.sort(0, True)
+    max_prob_id = torch.argmax(probs)
+    # display accuracy
+    print(probs[max_prob_id].item(), classes[idx[max_prob_id]])
 
 #print('{} prediction on {}'.format(arch,img_name))
 # output the prediction
